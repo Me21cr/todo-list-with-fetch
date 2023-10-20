6d22a8a63db31ba76fcb24ce0dbeddc24b9c0559
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-10-20 00:52:50 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,121 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+
+
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const API_URL= "https://playground.4geeks.com/apis/fake/todos/user/mery";
+const [tarea, setTarea] = useState({
+	"label": "",
+	"done": false
+});
+
+const [tareaLista, setTareaLista] = useState([]);
+
+const handleChange = (event) => {
+	setTarea({
+		...tarea,
+		"label": event.target.value
+	});
 };
 
+const handleDelete = (label) => {
+	const nuevaTareaLista = tareaLista.filter((item) => item.label !== label);
+	updateTareaLista(nuevaTareaLista);
+};
+
+const updateTareaLista = async (nuevaLista) => {
+	try {
+		let response = await fetch(API_URL, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify(nuevaLista)
+		});
+		if (response.ok) {
+			setTareaLista(nuevaLista);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const saveTarea = async (event) => {
+	if (event.key === "Enter") {
+		try {
+			let response = await fetch(API_URL, {
+				method: "PUT",
+				headers: {
+					"Content-type": "application/json"
+				},
+				body: JSON.stringify([...tareaLista, tarea])
+			});
+			if (response.ok) {
+				getTarea();
+				setTarea({
+					"label": "",
+					"done": false
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
+
+const getTarea = async () => {
+	try {
+		let response = await fetch(API_URL);
+		if (response.ok) {
+			let data = await response.json();
+			setTareaLista(data);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+useEffect(() => {
+	getTarea();
+}, []);
+
+return (
+	<div className="container">
+		<div className="row justify-content-center">
+			<div className="col-12 col-md-6">
+				<h2>Todo List</h2>
+				<input
+					type="text"
+					placeholder="Agrega la tarea"
+					className="form-control"
+					name="label"
+					value={tarea.label}
+					onChange={handleChange}
+					onKeyDown={saveTarea}
+				/>
+				<ul className="mt-4">
+					{tareaLista.map((item, index) => {
+						return (
+							<li className="d-flex justify-content-between"key={index}>{item.label}  
+							<button
+								className="btn btn-outline-danger btn-delete "
+								onClick={() => handleDelete(item.label)}
+							>
+						 x                                </button>
+						</li>
+						)
+					})}
+				</ul>
+			</div>
+		</div>
+	</div>
+   );
+}
+
+
+
 export default Home;
+
